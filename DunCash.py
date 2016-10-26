93a3725f834a3255b3d335df7e49a4a9263e5d2e
Samuel Shadwell <smshdwll@gmail.com>
2016-10-26 14:36:09 -0500

    Changed some stuff, works now I guess.

--- DunCash.py
+++ DunCash.py
@@ -1,6 +1,5 @@
 import argparse
 import csv
-import matplotlib.pyplot as plt
 import random
 from wordcloud import WordCloud
 
@@ -8,7 +7,7 @@ __author__ = 'github.com/samshadwell'
 
 
 def green_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-    return "hsl(120, 76%%, %d%%)" % random.randint(0, 60)
+    return "hsl(120, 76%%, %d%%)" % random.randint(10, 60)
 
 
 def main():
@@ -16,6 +15,11 @@ def main():
     Main function. Give the program the file of CSV to generate a word cloud from.
     :return: None.
     """
+    FONT_PATH = '/Library/Fonts/Arial.ttf'
+    HEIGHT = 700
+    WIDTH = 1600
+    BACKGROUND = 'white'
+
     parser = argparse.ArgumentParser()
     parser.add_argument("infile", help="The csv to generate a DunCash graphic from")
     parser.add_argument("outfile", help="The file to write the resulting word cloud to")
@@ -29,13 +33,10 @@ def main():
             out_freq = float(row[1][row[1].index('$') + 1:])
             word_totals.append((out_word, out_freq))
 
-        wc = WordCloud(height=300, width=1000, background_color="white", color_func=green_color_func)
+        wc = WordCloud(font_path=FONT_PATH, height=HEIGHT, width=WIDTH, background_color=BACKGROUND,
+                       color_func=green_color_func)
         wc.fit_words(word_totals)
-        #wc.to_file(args.outfile)
-
-        plt.imshow(wc)
-        plt.axis("off")
-        plt.show()
+        wc.to_file(args.outfile)
 
 
 if __name__ == "__main__":
